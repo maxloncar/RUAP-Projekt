261fc3288bbda634aeedcb7a905f1a074af2fe3d
maxloncar <loncarmax@gmail.com>
2020-12-04 18:02:44 +0100

    Visual Studio projekt - defaultni

---
+++ LV1 RUAP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LV1_RUAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
